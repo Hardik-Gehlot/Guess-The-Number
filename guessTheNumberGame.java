4d28b8c3f99f34344965d9ca5befb5921e6e0f5a
Hardik Gehlot <hardikgehlot2303@gmail.com>
2021-03-14 23:50:48 +0530

    added the challenge mode in the game

--- guessTheNumberGame.java
+++ guessTheNumberGame.java
@@ -5,9 +5,12 @@ class game{
     public int computerInput;
     public int userInput;
     public int noOfGuesses;
+    public int challengeNumber;
     game(){
         Random rand = new Random();
         this.computerInput=rand.nextInt(100);
+        this.challengeNumber=rand.nextInt(2)+5;
+        System.out.println("New Challenge -> Choose the Right Number in 0-"+challengeNumber+" attempts");
     }
     void takeUserInput(){
         System.out.println("Choose a Number");
@@ -17,7 +20,13 @@ class game{
     boolean isCorrect(){
         noOfGuesses++;
         if(userInput==computerInput){
-            System.out.format("Yes you guessed it Right, it was %d \n",computerInput);
+            System.out.format("Yes you guessed it Right, it was %d \n You guessed it in %d attempts\n",computerInput,noOfGuesses);
+            if(noOfGuesses<=challengeNumber){
+                System.out.println("Yeh! You Completed a Challenge");
+            }
+            else {
+                System.out.println("Oops! You lost the Challenge");
+            }
             return true;
         }
         else if (userInput<computerInput){
